221bd63440b55dfdac747fb9070947a5bc34b9d3
Manoj Srinivasan <ms14845@ga025.hpc.nyu.edu>
2024-04-10 22:07:32 -0400

    Dataloader modified

--- dataloader.py
+++ dataloader.py
@@ -36,12 +36,12 @@ class Cifar10Dataset(Dataset):
         if self.split == 'train':
             self.img_transform = transforms.Compose([
                         transforms.ToTensor(),
-                        # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                     ])
         elif self.split == 'test':
             self.img_transform =  transforms.Compose([
                         transforms.ToTensor(),
-                        # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                     ])
 
 
--- scrap.py
+++ scrap.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
+import pickle5 as pickle
 
 def unpickle(file):
-    import pickle
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+        dict1 = pickle.load(fo, encoding='bytes')
+    return dict1
 
-
-res = unpickle('dataset/cifar-10-python/cifar-10-batches-py/data_batch_2')
+# res = unpickle('dataset/cifar-10-python/cifar-10-batches-py/data_batch_2')
+res = unpickle('dataset/cifar_test_nolabels.pkl')
 # res = unpickle('dataset/cifar-10-python/cifar-10-batches-py/test_batch')
 print(res[b'data'].shape)
-num = 1
+num = 120
 
 first_pic = res[b'data'][num].reshape(3,32,32).transpose(1,2,0)
 print(first_pic.shape)
 print(res.keys())
-print(res[b'labels'][num])
-print(res[b'filenames'][num])
+print(res[b'ids'][num])
+# print(res[b'labels'][num])
+# print(res[b'filenames'][num])
 plt.imshow(first_pic)
 plt.savefig('scrap.png')
